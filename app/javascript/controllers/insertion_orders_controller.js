3fd32ca82bb9baefa3ff5af563103710ff1df46c
Nathan Hopkins <natehop@gmail.com>
2019-12-14 09:29:32 -0700

    Give reflex elements a unique id

--- app/javascript/controllers/insertion_orders_controller.js
+++ app/javascript/controllers/insertion_orders_controller.js
@@ -2,7 +2,6 @@ import ApplicationController from './application_controller'
 
 export default class extends ApplicationController {
   afterSetCampaignBudget (element) {
-    console.log('afterSetCampaignBudget', 'nate')
     const budget = element.value
     const { totalBudget } = element.closest('[data-total-budget]').dataset
     if (Number(budget) !== Number(totalBudget)) element.value = totalBudget
