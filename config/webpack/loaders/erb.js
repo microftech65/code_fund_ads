05cc29c5d630d14cc4cff23b9911e1e5cf07622a
Andrew Mason <andrewmcodes@protonmail.com>
2020-01-07 18:20:10 -0500

    fix module resolver

--- config/webpack/loaders/erb.js
+++ config/webpack/loaders/erb.js
@@ -1,9 +1,23 @@
+// module.exports = {
+//   test: /\.erb$/,
+//   enforce: 'pre',
+//   exclude: /node_modules/,
+//   use: [{
+//     loader: 'rails-erb-loader',
+//     options: {
+//       runner: (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner'
+//     }
+//   }]
+// }
+
+const path = require('path')
+
 module.exports = {
   test: /\.erb$/,
   enforce: 'pre',
   include: [
     path.resolve(__dirname, 'app/views/advertisements'),
-    path.resolve(__dirname, 'app/javascript/advertisements'),
+    path.resolve(__dirname, 'app/javascript/advertisements')
   ],
   exclude: [path.resolve(__dirname, 'node_modules')],
   use: [
