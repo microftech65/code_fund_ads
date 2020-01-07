72e6bde37dcc77d8f12984d0644326ae5fea0160
Andrew Mason <andrewmcodes@protonmail.com>
2020-01-07 17:59:16 -0500

    hmmm

--- config/webpack/loaders/erb.js
+++ config/webpack/loaders/erb.js
@@ -1,11 +1,18 @@
 module.exports = {
   test: /\.erb$/,
   enforce: 'pre',
-  exclude: /node_modules/,
-  use: [{
-    loader: 'rails-erb-loader',
-    options: {
-      runner: (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner'
+  include: [
+    path.resolve(__dirname, 'app/views/advertisements'),
+    path.resolve(__dirname, 'app/javascript/advertisements'),
+  ],
+  exclude: [path.resolve(__dirname, 'node_modules')],
+  use: [
+    {
+      loader: 'rails-erb-loader',
+      options: {
+        runner:
+          (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner'
+      }
     }
-  }]
+  ]
 }
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,12 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome
+
+  def teardown
+    Rails.cache.clear
+  end
+
+  def refresh
+    page.reset!
+  end
 end
