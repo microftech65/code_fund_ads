33f33a0bcca6577fcdc229b72feeacd561285619
Andrew Mason <andrewmcodes@protonmail.com>
2020-01-13 16:46:52 -0500

    add pricing and tests

--- db/views/regions_v03.sql
+++ db/views/regions_v03.sql
@@ -12,14 +12,14 @@
 select
 1 "id",
 'Africa' "name",
-'USD' "blockchain_ecpm_currency", 500 "blockchain_ecpm_cents",
-'USD' "css_and_design_ecpm_currency", 500 "css_and_design_ecpm_cents",
-'USD' "dev_ops_ecpm_currency", 500 "dev_ops_ecpm_cents",
-'USD' "game_development_ecpm_currency", 500 "game_development_ecpm_cents",
-'USD' "javascript_and_frontend_ecpm_currency", 500 "javascript_and_frontend_ecpm_cents",
-'USD' "miscellaneous_ecpm_currency", 500 "miscellaneous_ecpm_cents",
-'USD' "mobile_development_ecpm_currency", 500 "mobile_development_ecpm_cents",
-'USD' "web_development_and_backend_ecpm_currency", 500 "web_development_and_backend_ecpm_cents",
+'USD' "blockchain_ecpm_currency", 75 "blockchain_ecpm_cents",
+'USD' "css_and_design_ecpm_currency", 38 "css_and_design_ecpm_cents",
+'USD' "dev_ops_ecpm_currency", 53 "dev_ops_ecpm_cents",
+'USD' "game_development_ecpm_currency", 38 "game_development_ecpm_cents",
+'USD' "javascript_and_frontend_ecpm_currency", 68 "javascript_and_frontend_ecpm_cents",
+'USD' "miscellaneous_ecpm_currency", 23 "miscellaneous_ecpm_cents",
+'USD' "mobile_development_ecpm_currency", 38 "mobile_development_ecpm_cents",
+'USD' "web_development_and_backend_ecpm_currency", 45 "web_development_and_backend_ecpm_cents",
 '{AO,BF,BI,BJ,BW,CD,CF,CG,CI,CM,CV,DJ,DZ,EG,EH,ER,ET,GA,GH,GM,GN,GQ,GW,IO,KE,KM,LR,LS,LY,MA,MG,ML,MR,MU,MW,MZ,NA,NE,NG,RE,RW,SC,SD,SH,SL,SN,SO,SS,ST,SZ,TD,TG,TN,TZ,UG,YT,ZA,ZM,ZW}'::text[] "country_codes"
 
 union all
@@ -27,14 +27,14 @@ union all
 select
 2 "id",
 'Americas - Central and Southern',
-'USD', 500, -- blockchain
-'USD', 500, -- css_and_design
-'USD', 500, -- dev_ops
-'USD', 500, -- game_development
-'USD', 500, -- javascript_and_frontend
-'USD', 500, -- miscellaneous
-'USD', 500, -- mobile_development
-'USD', 500, -- web_development_and_backend
+'USD', 150, -- blockchain
+'USD', 75, -- css_and_design
+'USD', 105, -- dev_ops
+'USD', 75, -- game_development
+'USD', 135, -- javascript_and_frontend
+'USD', 45, -- miscellaneous
+'USD', 75, -- mobile_development
+'USD', 90, -- web_development_and_backend
 '{AR,BO,BR,BZ,CL,CO,CR,EC,FK,GF,GS,GT,GY,HN,MX,NI,PA,PE,PY,SR,SV,UY,VE}'::text[]
 
 union all
@@ -42,14 +42,14 @@ union all
 select
 3 "id",
 'Americas - Northern',
-'USD', 500, -- blockchain
-'USD', 500, -- css_and_design
-'USD', 500, -- dev_ops
-'USD', 500, -- game_development
-'USD', 500, -- javascript_and_frontend
-'USD', 500, -- miscellaneous
-'USD', 500, -- mobile_development
-'USD', 500, -- web_development_and_backend
+'USD', 750, -- blockchain
+'USD', 375, -- css_and_design
+'USD', 525, -- dev_ops
+'USD', 375, -- game_development
+'USD', 675, -- javascript_and_frontend
+'USD', 225, -- miscellaneous
+'USD', 375, -- mobile_development
+'USD', 450, -- web_development_and_backend
 '{US,CA}'::text[] "country_codes"
 
 union all
@@ -57,14 +57,14 @@ union all
 select
 4 "id",
 'Asia - Central and South-Eastern',
-'USD', 500, -- blockchain
-'USD', 500, -- css_and_design
-'USD', 500, -- dev_ops
-'USD', 500, -- game_development
-'USD', 500, -- javascript_and_frontend
-'USD', 500, -- miscellaneous
-'USD', 500, -- mobile_development
-'USD', 500, -- web_development_and_backend
+'USD', 225, -- blockchain
+'USD', 113, -- css_and_design
+'USD', 158, -- dev_ops
+'USD', 113, -- game_development
+'USD', 203, -- javascript_and_frontend
+'USD', 68, -- miscellaneous
+'USD', 113, -- mobile_development
+'USD', 135, -- web_development_and_backend
 '{BN,ID,KG,KH,KZ,LA,MM,MY,PH,SG,TH,TJ,TL,TM,UZ,VN}'::text[]
 
 union all
@@ -72,14 +72,14 @@ union all
 select
 5 "id",
 'Asia - Eastern',
-'USD', 500, -- blockchain
-'USD', 500, -- css_and_design
-'USD', 500, -- dev_ops
-'USD', 500, -- game_development
-'USD', 500, -- javascript_and_frontend
-'USD', 500, -- miscellaneous
-'USD', 500, -- mobile_development
-'USD', 500, -- web_development_and_backend
+'USD', 225, -- blockchain
+'USD', 113, -- css_and_design
+'USD', 158, -- dev_ops
+'USD', 113, -- game_development
+'USD', 203, -- javascript_and_frontend
+'USD', 68, -- miscellaneous
+'USD', 113, -- mobile_development
+'USD', 135, -- web_development_and_backend
 '{CN,HK,JP,KP,KR,MN,MO,TW}'::text[]
 
 union all
@@ -87,14 +87,14 @@ union all
 select
 6 "id",
 'Asia - Southern and Western',
-'USD', 500, -- blockchain
-'USD', 500, -- css_and_design
-'USD', 500, -- dev_ops
-'USD', 500, -- game_development
-'USD', 500, -- javascript_and_frontend
-'USD', 500, -- miscellaneous
-'USD', 500, -- mobile_development
-'USD', 500, -- web_development_and_backend
+'USD', 225, -- blockchain
+'USD', 113, -- css_and_design
+'USD', 158, -- dev_ops
+'USD', 113, -- game_development
+'USD', 203, -- javascript_and_frontend
+'USD', 68, -- miscellaneous
+'USD', 113, -- mobile_development
+'USD', 135, -- web_development_and_backend
 '{AE,AF,AM,AZ,BD,BH,BT,CY,GE,IL,IN,IQ,IR,JO,KW,LB,LK,MV,NP,OM,PK,PS,QA,SA,SY,TR,YE}'::text[]
 
 union all
@@ -102,14 +102,14 @@ union all
 select
 7 "id",
 'Australia and New Zealand',
-'USD', 500, -- blockchain
-'USD', 500, -- css_and_design
-'USD', 500, -- dev_ops
-'USD', 500, -- game_development
-'USD', 500, -- javascript_and_frontend
-'USD', 500, -- miscellaneous
-'USD', 500, -- mobile_development
-'USD', 500, -- web_development_and_backend
+'USD', 750, -- blockchain
+'USD', 375, -- css_and_design
+'USD', 525, -- dev_ops
+'USD', 375, -- game_development
+'USD', 675, -- javascript_and_frontend
+'USD', 225, -- miscellaneous
+'USD', 375, -- mobile_development
+'USD', 450, -- web_development_and_backend
 '{AU,CC,CX,NF,NZ}'::text[]
 
 union all
@@ -117,14 +117,14 @@ union all
 select
 8 "id",
 'Europe',
-'USD', 500, -- blockchain
-'USD', 500, -- css_and_design
-'USD', 500, -- dev_ops
-'USD', 500, -- game_development
-'USD', 500, -- javascript_and_frontend
-'USD', 500, -- miscellaneous
-'USD', 500, -- mobile_development
-'USD', 500, -- web_development_and_backend
+'USD', 675, -- blockchain
+'USD', 338, -- css_and_design
+'USD', 473, -- dev_ops
+'USD', 338, -- game_development
+'USD', 608, -- javascript_and_frontend
+'USD', 203, -- miscellaneous
+'USD', 338, -- mobile_development
+'USD', 405, -- web_development_and_backend
 '{AD,AL,AT,AX,BA,BE,CH,DE,DK,EE,ES,FI,FO,FR,GB,GG,GI,GR,HR,IE,IM,IS,IT,JE,LI,LT,LU,LV,MC,ME,MK,MT,NL,NO,PT,RS,SE,SI,SJ,SM,VA}'::text[]
 
 union all
@@ -132,14 +132,14 @@ union all
 select
 9 "id",
 'Europe - Eastern',
-'USD', 500, -- blockchain
-'USD', 500, -- css_and_design
-'USD', 500, -- dev_ops
-'USD', 500, -- game_development
-'USD', 500, -- javascript_and_frontend
-'USD', 500, -- miscellaneous
-'USD', 500, -- mobile_development
-'USD', 500, -- web_development_and_backend
+'USD', 450, -- blockchain
+'USD', 225, -- css_and_design
+'USD', 315, -- dev_ops
+'USD', 225, -- game_development
+'USD', 405, -- javascript_and_frontend
+'USD', 135, -- miscellaneous
+'USD', 225, -- mobile_development
+'USD', 270, -- web_development_and_backend
 '{BG,BY,CZ,HU,MD,PL,RO,RU,SK,UA}'::text[]
 
 union all
@@ -147,12 +147,12 @@ union all
 select
 10,
 'Other',
-'USD', 600, -- blockchain
-'USD', 50,  -- css and design
-'USD', 250, -- dev ops
-'USD', 25,  -- game development
-'USD', 225, -- javascript
-'USD', 25,  -- miscellaneous
-'USD', 50,  -- mobile development
-'USD', 100, -- web development
+'USD', 75, -- blockchain
+'USD', 38,  -- css and design
+'USD', 53, -- dev ops
+'USD', 38,  -- game development
+'USD', 68, -- javascript
+'USD', 23,  -- miscellaneous
+'USD', 38,  -- mobile development
+'USD', 45, -- web development
 '{AG,AI,AS,AW,BB,BL,BM,BQ,BS,CK,CU,CW,DM,DO,FJ,FM,GD,GL,GP,GU,HT,JM,KI,KN,KY,LC,MF,MH,MP,MQ,MS,NC,NR,NU,PF,PG,PM,PN,PR,PW,SB,SX,TC,TK,TO,TT,TV,UM,VC,VG,VI,VU,WF,WS}'::text[]
--- test/models/region_test.rb
+++ test/models/region_test.rb
@@ -27,47 +27,149 @@ require "test_helper"
 
 class RegionTest < ActiveSupport::TestCase
   test "ecpm pricing for audience" do
-    assert Region.find(1).ecpm(Audience.blockchain).format == "$10.00"
-    assert Region.find(2).ecpm(Audience.mobile_development).format == "$3.50"
-    assert Region.find(3).ecpm(Audience.dev_ops).format == "$2.50"
+    assert Region.find(1).ecpm(Audience.blockchain).format == "$0.75"
+    assert Region.find(2).ecpm(Audience.mobile_development).format == "$0.75"
+    assert Region.find(3).ecpm(Audience.dev_ops).format == "$5.25"
+    assert Region.find(4).ecpm(Audience.game_development).format == "$1.13"
+    assert Region.find(5).ecpm(Audience.javascript_and_frontend).format == "$2.03"
+    assert Region.find(6).ecpm(Audience.web_development_and_backend).format == "$1.35"
+    assert Region.find(7).ecpm(Audience.miscellaneous).format == "$2.25"
+    assert Region.find(8).ecpm(Audience.css_and_design).format == "$3.38"
+    assert Region.find(9).ecpm(Audience.blockchain).format == "$4.50"
+    assert Region.find(10).ecpm(Audience.mobile_development).format == "$0.38"
   end
 
-  test "region one pricing" do
-    region = Region.find(1)
-
-    assert region.blockchain_ecpm.format == "$10.00"
-    assert region.css_and_design_ecpm.format == "$4.50"
-    assert region.dev_ops_ecpm.format == "$6.50"
-    assert region.game_development_ecpm.format == "$4.25"
-    assert region.javascript_and_frontend_ecpm.format == "$6.25"
-    assert region.miscellaneous_ecpm.format == "$4.25"
-    assert region.mobile_development_ecpm.format == "$4.50"
-    assert region.web_development_and_backend_ecpm.format == "$5.00"
+  test "all country codes are used" do
+    Region.all.map(&:country_codes).flatten == Country.all.map(&:iso_code)
   end
 
-  test "region two pricing" do
-    region = Region.find(2)
+  test "Africa pricing" do
+    region = Region.africa
 
-    assert region.blockchain_ecpm.format == "$9.00"
-    assert region.css_and_design_ecpm.format == "$3.50"
-    assert region.dev_ops_ecpm.format == "$5.50"
-    assert region.game_development_ecpm.format == "$3.25"
-    assert region.javascript_and_frontend_ecpm.format == "$5.25"
-    assert region.miscellaneous_ecpm.format == "$3.25"
-    assert region.mobile_development_ecpm.format == "$3.50"
-    assert region.web_development_and_backend_ecpm.format == "$4.00"
+    assert region.blockchain_ecpm.format == "$0.75"
+    assert region.css_and_design_ecpm.format == "$0.38"
+    assert region.dev_ops_ecpm.format == "$0.53"
+    assert region.game_development_ecpm.format == "$0.38"
+    assert region.javascript_and_frontend_ecpm.format == "$0.68"
+    assert region.miscellaneous_ecpm.format == "$0.23"
+    assert region.mobile_development_ecpm.format == "$0.38"
+    assert region.web_development_and_backend_ecpm.format == "$0.45"
   end
 
-  test "region three pricing" do
-    region = Region.find(3)
+  test "Americas - Central and Southern pricing" do
+    region = Region.americas_central_southern
 
-    assert region.blockchain_ecpm.format == "$6.00"
-    assert region.css_and_design_ecpm.format == "$0.50"
-    assert region.dev_ops_ecpm.format == "$2.50"
-    assert region.game_development_ecpm.format == "$0.25"
-    assert region.javascript_and_frontend_ecpm.format == "$2.25"
-    assert region.miscellaneous_ecpm.format == "$0.25"
-    assert region.mobile_development_ecpm.format == "$0.50"
-    assert region.web_development_and_backend_ecpm.format == "$1.00"
+    assert region.blockchain_ecpm.format == "$1.50"
+    assert region.css_and_design_ecpm.format == "$0.75"
+    assert region.dev_ops_ecpm.format == "$1.05"
+    assert region.game_development_ecpm.format == "$0.75"
+    assert region.javascript_and_frontend_ecpm.format == "$1.35"
+    assert region.miscellaneous_ecpm.format == "$0.45"
+    assert region.mobile_development_ecpm.format == "$0.75"
+    assert region.web_development_and_backend_ecpm.format == "$0.90"
+  end
+
+  test "Americas - Northern pricing" do
+    region = Region.americas_northern
+
+    assert region.blockchain_ecpm.format == "$7.50"
+    assert region.css_and_design_ecpm.format == "$3.75"
+    assert region.dev_ops_ecpm.format == "$5.25"
+    assert region.game_development_ecpm.format == "$3.75"
+    assert region.javascript_and_frontend_ecpm.format == "$6.75"
+    assert region.miscellaneous_ecpm.format == "$2.25"
+    assert region.mobile_development_ecpm.format == "$3.75"
+    assert region.web_development_and_backend_ecpm.format == "$4.50"
+  end
+
+  test "Asia - Central and South-Eastern pricing" do
+    region = Region.asia_central_and_south_eastern
+
+    assert region.blockchain_ecpm.format == "$2.25"
+    assert region.css_and_design_ecpm.format == "$1.13"
+    assert region.dev_ops_ecpm.format == "$1.58"
+    assert region.game_development_ecpm.format == "$1.13"
+    assert region.javascript_and_frontend_ecpm.format == "$2.03"
+    assert region.miscellaneous_ecpm.format == "$0.68"
+    assert region.mobile_development_ecpm.format == "$1.13"
+    assert region.web_development_and_backend_ecpm.format == "$1.35"
+  end
+
+  test "Asia - Eastern pricing" do
+    region = Region.asia_eastern
+
+    assert region.blockchain_ecpm.format == "$2.25"
+    assert region.css_and_design_ecpm.format == "$1.13"
+    assert region.dev_ops_ecpm.format == "$1.58"
+    assert region.game_development_ecpm.format == "$1.13"
+    assert region.javascript_and_frontend_ecpm.format == "$2.03"
+    assert region.miscellaneous_ecpm.format == "$0.68"
+    assert region.mobile_development_ecpm.format == "$1.13"
+    assert region.web_development_and_backend_ecpm.format == "$1.35"
+  end
+
+  test "Asia - Southern and Western pricing" do
+    region = Region.asia_southern_and_western
+
+    assert region.blockchain_ecpm.format == "$2.25"
+    assert region.css_and_design_ecpm.format == "$1.13"
+    assert region.dev_ops_ecpm.format == "$1.58"
+    assert region.game_development_ecpm.format == "$1.13"
+    assert region.javascript_and_frontend_ecpm.format == "$2.03"
+    assert region.miscellaneous_ecpm.format == "$0.68"
+    assert region.mobile_development_ecpm.format == "$1.13"
+    assert region.web_development_and_backend_ecpm.format == "$1.35"
+  end
+
+  test "Australia and New Zealand pricing" do
+    region = Region.australia_and_new_zealand
+
+    assert region.blockchain_ecpm.format == "$7.50"
+    assert region.css_and_design_ecpm.format == "$3.75"
+    assert region.dev_ops_ecpm.format == "$5.25"
+    assert region.game_development_ecpm.format == "$3.75"
+    assert region.javascript_and_frontend_ecpm.format == "$6.75"
+    assert region.miscellaneous_ecpm.format == "$2.25"
+    assert region.mobile_development_ecpm.format == "$3.75"
+    assert region.web_development_and_backend_ecpm.format == "$4.50"
+  end
+
+  test "Europe pricing" do
+    region = Region.europe
+
+    assert region.blockchain_ecpm.format == "$6.75"
+    assert region.css_and_design_ecpm.format == "$3.38"
+    assert region.dev_ops_ecpm.format == "$4.73"
+    assert region.game_development_ecpm.format == "$3.38"
+    assert region.javascript_and_frontend_ecpm.format == "$6.08"
+    assert region.miscellaneous_ecpm.format == "$2.03"
+    assert region.mobile_development_ecpm.format == "$3.38"
+    assert region.web_development_and_backend_ecpm.format == "$4.05"
+  end
+
+  test "Europe - Eastern pricing" do
+    region = Region.europe_eastern
+
+    assert region.blockchain_ecpm.format == "$4.50"
+    assert region.css_and_design_ecpm.format == "$2.25"
+    assert region.dev_ops_ecpm.format == "$3.15"
+    assert region.game_development_ecpm.format == "$2.25"
+    assert region.javascript_and_frontend_ecpm.format == "$4.05"
+    assert region.miscellaneous_ecpm.format == "$1.35"
+    assert region.mobile_development_ecpm.format == "$2.25"
+    assert region.web_development_and_backend_ecpm.format == "$2.70"
+  end
+
+  test "Other pricing" do
+    region = Region.other
+
+    assert region.blockchain_ecpm.format == "$0.75"
+    assert region.css_and_design_ecpm.format == "$0.38"
+    assert region.dev_ops_ecpm.format == "$0.53"
+    assert region.game_development_ecpm.format == "$0.38"
+    assert region.javascript_and_frontend_ecpm.format == "$0.68"
+    assert region.miscellaneous_ecpm.format == "$0.23"
+    assert region.mobile_development_ecpm.format == "$0.38"
+    assert region.web_development_and_backend_ecpm.format == "$0.45"
   end
 end
